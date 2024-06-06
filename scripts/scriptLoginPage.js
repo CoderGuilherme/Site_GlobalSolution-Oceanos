29c30b902a3dfe03bfabdfa751e04bf53d7275b0
Guilherme oliveira Da silva <guilhermesoliveira20@gmail.com>
2024-06-06 20:39:11 -0300

    Develop: verificacao de usuario - LoginPage

--- scripts/scriptLoginPage.js
+++ scripts/scriptLoginPage.js
@@ -3,12 +3,17 @@ function Login(){
     const senha = parseInt(document.getElementById('senha_input').value);
     const usuario = document.getElementById('usuario_input').value;
 
-    if (senha === 1234){
+    if (senha === 1234 && usuario === "admin"){
         alert('Acesso liberado');
         location.href = "index.html"
     }
+    else if(senha != 1234){
+        alert('Senha incorreta');}
+    else if(usuario != "admin"){
+        alert('Usuario incorreto')
+    }
     else{
-        alert('Senha incorreta');
+        alert('login incorreto')
     }
 
 }
