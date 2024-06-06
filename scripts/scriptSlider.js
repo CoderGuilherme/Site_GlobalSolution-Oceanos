eac035c11bcb0f6570c7a8778c09233615aa6e19
Guilherme oliveira Da silva <guilhermesoliveira20@gmail.com>
2024-06-06 20:49:31 -0300

    Develop: links ajustados 2.0

--- scripts/scriptSlider.js
+++ scripts/scriptSlider.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
     // Dicionário de imagens e palavras
     const dictionary = {
-        "SOLUÇÃO": ["/image/prototipo.jpg","O dispositivo de IoT projetado para embarcações filtra microplásticos e nanoplásticos enquanto captura plásticos maiores. Além disso, ele coleta e armazena dados para monitorar a qualidade da água durante a navegação."],
-        "Filtro": ["/image/filtro.jpeg", "O filtro é a primeira etapa do funil. <br> Ele utiliza uma tecnologia eletromagnética por meio de uma mistura de pó de magnetita e óleo vegetal para atrair os microplásticos."],
-        "Visor": ["/image/visor.jpg", "O visor é a parte intermediária do funil. Ele permite visualizar os dados filtrados de forma mais detalhada, mantendo um histórico dos locais com maiores índices de microplásticos e resíduos coletados."],
-        "Sensor": ["/image/sensor.jpeg", "O sensor é a etapa do funil que serve como coleta de dados e monitoramento. Ele monitora continuamente os dados e detecta mudanças no pH da água causadas pelo acúmulo de microplásticos."],
-        "Cilindro de Armazenamento": ["/image/cilindro.jpeg", "O Cilindro de Armazenamento é a última etapa. Serve como depósito de todos os resíduos coletados. É possível acoplar outros tamanhos de cilindro no dispositivo."]
+        "SOLUÇÃO": ["image/prototipo.jpg","O dispositivo de IoT projetado para embarcações filtra microplásticos e nanoplásticos enquanto captura plásticos maiores. Além disso, ele coleta e armazena dados para monitorar a qualidade da água durante a navegação."],
+        "Filtro": ["image/filtro.jpeg", "O filtro é a primeira etapa do funil. <br> Ele utiliza uma tecnologia eletromagnética por meio de uma mistura de pó de magnetita e óleo vegetal para atrair os microplásticos."],
+        "Visor": ["image/visor.jpg", "O visor é a parte intermediária do funil. Ele permite visualizar os dados filtrados de forma mais detalhada, mantendo um histórico dos locais com maiores índices de microplásticos e resíduos coletados."],
+        "Sensor": ["image/sensor.jpeg", "O sensor é a etapa do funil que serve como coleta de dados e monitoramento. Ele monitora continuamente os dados e detecta mudanças no pH da água causadas pelo acúmulo de microplásticos."],
+        "Cilindro de Armazenamento": ["image/cilindro.jpeg", "O Cilindro de Armazenamento é a última etapa. Serve como depósito de todos os resíduos coletados. É possível acoplar outros tamanhos de cilindro no dispositivo."]
     };
 
     const words = Object.keys(dictionary);
