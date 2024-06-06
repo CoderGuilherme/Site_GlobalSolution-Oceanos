eac363f049278d32afd095126087c9fb5a875e5f
Guilherme oliveira Da silva <guilhermesoliveira20@gmail.com>
2024-06-06 04:09:53 -0300

    Feat: ContactUsPage criada

---
+++ scripts/scriptContact.js
@@ -0,0 +1,8 @@
+
+
+function ColectInfo(){
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+    alert(`EMAIL ENVIADO! \n \n Nome: ${name} \n Email: ${email} \n Menssagem: ${message}`)
+}
