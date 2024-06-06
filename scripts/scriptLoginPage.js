3efeacf581e371169f0da9b780bf4e29966f0b78
Guilherme oliveira Da silva <guilhermesoliveira20@gmail.com>
2024-06-06 04:29:21 -0300

    Feat: Login Page Criada

---
+++ scripts/scriptLoginPage.js
@@ -0,0 +1,14 @@
+
+function Login(){   
+    const senha = parseInt(document.getElementById('senha_input').value);
+    const usuario = document.getElementById('usuario_input').value;
+
+    if (senha === 1234){
+        alert('Acesso liberado');
+        location.href = "index.html"
+    }
+    else{
+        alert('Senha incorreta');
+    }
+
+}
